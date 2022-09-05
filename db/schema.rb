1a33766eec73a486683b633abf8843f2a330ee31
Robert Low <robert.low91@gmail.com>
2022-09-05 17:24:39 +0100

    Venue model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_151928) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_161930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,4 +26,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_151928) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "venues", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "location"
+    t.string "name"
+    t.string "photos"
+    t.string "venue"
+    t.text "about"
+    t.string "website"
+    t.string "facebook"
+    t.string "instagram"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_venues_on_user_id"
+  end
+
+  add_foreign_key "venues", "users"
 end
