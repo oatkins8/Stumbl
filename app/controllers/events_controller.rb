36131b65ac0567268cda8137838e3832bdb37889
Alex DiCarlo <alexdicarlo.biz@gmail.com>
2022-09-06 11:59:50 +0100

    finished basic crud actions

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,43 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to event_path(@event)
+  end
+
+  private
+
+  def event_params
+    parmas.require(:event).premit(:date, :time, :price, :genre, :category, :producer, :name, :image, :about, :mini_description, :tickets_available, :cash, :card)
+  end
+end
