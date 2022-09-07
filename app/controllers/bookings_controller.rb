e9ae35e51c397683054816d59923ad52af25678e
Robert Low <robert.low91@gmail.com>
2022-09-07 12:12:11 +0100

    Booking create method created

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,16 @@
 class BookingsController < ApplicationController
 
-
+  def create
+    @booking = Booking.new(booking_params)
+    @event = Event.find(params[:event_id])
+    @booking.user = current_user
+    @booking.event = @event
+    if @booking.save
+      redirect_to event_path(@event)
+    else
+      render "events/show", status: :unprocessable_entity
+    end
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "events#index"
   resources :events, only: [:index, :show] do
-    #  resources :bookings, only: [:]
+    resources :bookings, only: [:create]
   end
   resources :venues, except: [:index] do
     resources :events, only: [:new, :create, :edit, :update, :destroy]
