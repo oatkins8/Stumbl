0f60a40341b0086ec7ee5f8f69179e50bcb9a7fc
rupert sealey <rupertsealey@hotmail.com>
2022-09-08 17:06:03 +0100

    user profile page started

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
 
   def show
-    @user = current_user
     @bookings = Booking.where(user_id: params[:id])
     @venues = Venue.where(user_id: params[:id])
-    authorize @user
+    authorize current_user
   end
 end
