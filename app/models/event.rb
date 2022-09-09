a2f7effc0d306f9ab88a2caef3e932a7ec7a596b
Oliver Atkins <ollieatkins8@outlook.com>
2022-09-09 13:03:04 +0100

    coudinary images

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   has_many :bookings
   belongs_to :venue
   include PgSearch::Model
+  has_many_attached :images
   pg_search_scope :global_search,
                   against: [ :name, :category, :genre, :producer ],
                   associated_against: {
