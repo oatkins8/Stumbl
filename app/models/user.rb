05a2bba8532d945c3cfdc5930255340b779c071c
Oliver Atkins <ollieatkins8@outlook.com>
2022-09-09 16:17:10 +0100

    final changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :venues
+  has_many :venues, dependent: :destroy
   has_many :events, through: :venues
   has_many :events, through: :bookings
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 end
