be5609d948b248c037e45c417d4120c26f0e60ba
Robert Low <robert.low91@gmail.com>
2022-09-12 10:23:13 +0100

    Model created for favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  acts_as_favoritor
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :venues, dependent: :destroy
