4cb0aafa0e292d70e93261519f7aca0747005011
Robert Low <robert.low91@gmail.com>
2022-09-12 14:45:41 +0100

    Favourites function completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  acts_as_favoritor
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :venues, dependent: :destroy
   has_many :events, through: :venues
   has_many :events, through: :bookings
   has_many :bookings, dependent: :destroy
+  acts_as_favoritor
 end
