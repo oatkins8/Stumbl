a09fd9921094d2b172ad737530d49c092f5c6b1c
Oliver Atkins <ollieatkins8@outlook.com>
2022-09-15 13:24:20 +0100

    final search styling

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -12,7 +12,7 @@ export default class extends Controller {
     this.geocoder = new MapboxGeocoder({
       accessToken: this.apiKeyValue,
       types: "country,region,place,postcode,locality,neighborhood,address",
-      placeholder: "loaction"
+      placeholder: "Loction"
     })
     this.geocoder.addTo(this.element)
     this.geocoder.on("result", event => this.#setInputValue(event))
