04c8cf3322be1fb721917b565c972964b84b164d
Oliver Atkins <ollieatkins8@outlook.com>
2022-11-27 07:26:13 +0000

    added selenium to test

--- test/system/venues_test.rb
+++ test/system/venues_test.rb
@@ -1,7 +1,7 @@
 require "application_system_test_case"
 
 class VenuesTest < ApplicationSystemTestCase
-  test "A signed in user can create a venue and an event" do
+  test "A signed in user can create a venue" do
     visit "users/sign_in"
     login_as users(:alex)
     visit "users/:id"
@@ -20,35 +20,10 @@ class VenuesTest < ApplicationSystemTestCase
     attach_file "venue_photos", "#{Rails.root}/test/fixtures/files/saints-coffee2.jpeg"
 
     click_on "Submit"
-    # visit "users/:id"
-    # click_on "My Venues"
     assert_text "Saints Coffee"
 
     visit "users/:id"
     click_on "My Venues"
     assert_selector "h2", text: "Saints Coffee"
-
-    # visit "events"
-    # save_and_open_screenshot
-
-    # assert_selector "mapboxgl-popup", count: Venue.count
   end
 end
-
-
-# test "lets a signed in user create a new product" do
-#   login_as users(:george) # Warden::TestHelpers (see test_helper.rb)
-#   visit "/products/new"
-
-#   fill_in "product_name", with: "Le Wagon"
-#   fill_in "product_tagline", with: "Change your life: Learn to code"
-
-
-#   click_on "Create Product"
-
-
-#   # should be directed back to the Home with a new product
-#   assert_text "Change your life: Learn to code"
-# end
-
-# end
